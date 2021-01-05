3c6b0d1e5ada32bcdf61cfcddb3c9990735cd5d9
FoxOdoo <foxodoo@gmail.com>
2021-01-05 16:23:48 +0100

    Iniciando proyecto

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Correo desde Contactos",
+    'summary': """
+        Formulario de Correo desde Contactos de Clientes.
+        """,
+    'description': """
+        Añade botón para acceder al formulario para redactar correos desde los contactos de clientes.
+    """,
+    'version': '12.0.1.0',
+    'author': "Flash Software",
+    'website': "http://www.flasof.com",
+    'category': 'Customizations',
+    'license': 'LGPL-3',
+    'support': 'foxodoo@gmail.com',
+    'depends': ['crm'],
+    'data': [
+        'views/customer_form.xml',
+    ],
+    'images': [
+        'static/description/icon.png',
+        'images/main_screenshot.png'
+    ],
+   
+}
