5288cbaf12976dcbd2564c3a86fec3d1b16a6c06
FoxOdoo <foxodoo@gmail.com>
2021-01-13 16:34:09 +0100

    Manifest cambio autor

--- mail_from_contacts/__manifest__.py
+++ mail_from_contacts/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Mail from Contacts",
+    'name': "Mail From Contacts",
     'summary': """
-        Mail Form from Customer Contacts.
-        """,
+        Mail Form From Customer Contacts.
+    """,
     'description': """
         Add button to access the form to compose emails from customer contacts.
         Only tested in CE.
     """,
-    'version': '12.0.1.1',
-    'author': "Flash Software",
+    'version': '14.0.1.0',
+    'author': "FlashOdoo",
     'website': "http://www.flashodoo.com",
     'category': 'Customizations',
     'license': 'LGPL-3',
